cd08a38aa4c21a746c5bb89f5ac333afe6f05057
DipuKP2001 <dipukp602@gmail.com>
2020-11-11 19:49:17 +0530

    backend folder structure

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,22 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-
 const app = express();
 
 app.use(bodyParser.json({ limit:"30mb",extended:true }))
 app.use(bodyParser.urlencoded({ limit:"30mb",extended:true }))
 app.use(cors())
 
-// MongoDB
+// Routes
+
+import postRoutes from './routes/posts.js'
+
+app.use('/posts',postRoutes)
+
+// MongoDB 
 
 const CONNECTION_URL = 'mongodb+srv://dipzzz234:br9PXPnWbrFLFiIP@cluster0.qdre7.mongodb.net/<dbname>?retryWrites=true&w=majority'
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser:true, useUnifiedTopology:true })
     .then(()=>app.listen(PORT,()=>console.log(`server running on port ${PORT}`)))
