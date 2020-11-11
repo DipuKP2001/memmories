3531a6d1ccfd251741f83c1eb3573f313d49d8b8
DipuKP2001 <dipukp602@gmail.com>
2020-11-11 20:05:13 +0530

    createPost and getPost

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { getPost } from '../controllers/posts.js'
+import { createPost, getPost } from '../controllers/posts.js'
 
 const router = express.Router()
 
 router.get('/',getPost)
+router.post('/',createPost)
 
 export default router
