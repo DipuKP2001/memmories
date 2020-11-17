6176858967fd3ee3f6b9f152a8d558a0b9b3b4ed
DipuKP2001 <dipukp602@gmail.com>
2020-11-17 12:31:15 +0530

    post styling completed

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -15,7 +15,7 @@ function Post({ post }){
     const classes = useStyles()
     return(
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia>
+            <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title}></CardMedia>
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
--- frontend/src/components/Posts/Post/styles.js
+++ frontend/src/components/Posts/Post/styles.js
@@ -1,3 +1,52 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-export default makeStyles(()=>{})
+export default makeStyles({
+    media:{
+        height: 0,
+        paddingTop:'56.25%',
+        backgroundColor:'rgba(0,0,0,0.5)',
+        backgroundBlendMode:'darken'
+    },
+    border:{
+        border:'solid'
+    },
+    fullHeightCard:{
+        height:'100%'
+    },
+    card:{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-between',
+        height:'100%',
+        borderRadius:'15px',
+        position:'relative'
+    },
+    overlay:{
+        position:'absolute',
+        top:'20px',
+        left:'20px',
+        color:'white'
+    },
+    overlay2:{
+        position:'absolute',
+        top:'20px',
+        right:'10px',
+        color:'white'
+    },
+    grid:{
+        display:'flex'
+    },
+    details:{
+        display:'flex',
+        justifyContent:'space-between',
+        margin:'20px'
+    },
+    title:{
+        padding:'0 16px'
+    },
+    cardActions:{
+        display:'flex',
+        justifyContent:'space-between',
+        padding:'0 16px 8px 16px'
+    }
+})
