2e87eeaac38d85c7e85308f2a94aec5645b6daac
DipuKP2001 <dipukp602@gmail.com>
2020-11-17 12:39:38 +0530

    posts styling completed

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -10,9 +10,9 @@ const Posts = ()=>{
     const classes = useStyles()
     return (        
         !posts.length ? <CircularProgress /> : (
-            <Grid className={classes.container} container>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {posts.map((post) => (
-                    <Grid key={post._id} item>
+                    <Grid key={post._id} item xs={12} sm={6}>
                         <Post post={post}/>
                     </Grid>
                 ))}
--- frontend/src/components/Posts/styles.js
+++ frontend/src/components/Posts/styles.js
@@ -1,5 +1,14 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-export default makeStyles(()=>{
-    
-})
+export default makeStyles((theme)=>({
+    mainContainer:{
+        dispay:'flex',
+        alignItems:'center'
+    },
+    smMargin:{
+        margin:theme.spacing(1)
+    },
+    actionDiv:{
+        textAlign:'center'
+    }
+}))
