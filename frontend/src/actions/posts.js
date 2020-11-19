9474987d1c10ac6fc726c080a560ae09c927710a
DipuKP2001 <dipukp602@gmail.com>
2020-11-19 15:36:26 +0530

    todo dispatch Post

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -28,3 +28,12 @@ export const updatePost = (id,post)=>async(dispatch)=>{
         console.log(error)
     }
 }
+
+export const deletePost = (id)=>async(dispatch)=>{
+    try {
+        await api.deletePost(id)
+        dispatch({ type:'DELETE',payload:id })
+    } catch (error) {
+        console.log(error)
+    }
+}
