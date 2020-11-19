4c2936a721c78faedaa2b5c2a0da8455da2142c1
DipuKP2001 <dipukp602@gmail.com>
2020-11-20 00:41:31 +0530

    likePost backend

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -46,3 +46,14 @@ export const deletePost = async(req,res)=>{
     await PostMessage.findByIdAndRemove(id)
     res.json({ message:'Post deleted succesfully' })
 }
+
+export const likePost = async(req,res)=>{
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send("No post with that id")
+
+    const post = await PostMessage.findById(id)
+    const updatedPost = await PostMessage.findByIdAndUpdate(id,{ likeCount : post.likeCount + 1 },{ new:true })
+
+    res.json(updatedPost)
+}
