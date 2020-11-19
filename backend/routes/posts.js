de4a9b32196d15ecc0beec85fe937c746c911e31
DipuKP2001 <dipukp602@gmail.com>
2020-11-19 15:24:29 +0530

    deletePost backend completed

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
-import { createPost, getPost, updatePost } from '../controllers/posts.js'
+import { createPost, getPost, updatePost, deletePost } from '../controllers/posts.js'
 
 const router = express.Router()
 
 router.get('/',getPost)
 router.post('/',createPost)
 router.patch('/:id',updatePost)
+router.delete('/:id',deletePost)
 
 export default router
