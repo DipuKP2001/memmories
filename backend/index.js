f2932f03afac496f18ab2b5e053c43f453eedbcf
DipuKP2001 <dipukp602@gmail.com>
2020-11-20 20:14:08 +0530

    action constants and env encryption

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
+import dotenv from 'dotenv'
 // Routes
 import postRoutes from './routes/posts.js'
 
-const app = express();
+const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit:"30mb",extended:true }))
 app.use(bodyParser.urlencoded({ limit:"30mb",extended:true }))
@@ -16,12 +17,10 @@ app.use(cors())
 
 app.use('/posts',postRoutes)
 
-// MongoDB 
 
-const CONNECTION_URL = 'mongodb+srv://dipzzz234:br9PXPnWbrFLFiIP@cluster0.qdre7.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{ useNewUrlParser:true, useUnifiedTopology:true })
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser:true, useUnifiedTopology:true })
     .then(()=>app.listen(PORT,()=>console.log(`server running on port ${PORT}`)))
     .catch((err)=>console.log(err))
 
